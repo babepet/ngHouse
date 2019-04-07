a793f0ee4f63a6812f845b8a341482b8d4492a0e
lemon <sendtolemon@163.com>
2019-04-08 07:45:34 +0800

    pkg static copy

--- src/app/app.js
+++ src/app/app.js
@@ -7,11 +7,26 @@ import "@uirouter/angularjs"
 var app = angular.module('myApp',['ui.router']);
 app.config(function ($stateProvider, $urlRouterProvider) {
     //一级默认
-    $urlRouterProvider.otherwise('/home'); 
+    $urlRouterProvider.otherwise('/'); 
     $stateProvider
         .state('home',{
             name:'home',
             url:'/home',
-            template:'<p>hello</p>'
+            templateUrl:'/pages/home/home.html',
+            controller: HomeCtrl
+        })
+        .state('home.editor',{
+            name:'editor',
+            url:'/editor',
+            templateUrl:'/pages/editor/editor.html',
+        })
+        .state('home.pushed',{
+            name:'pushed',
+            url:'/pushed',
+            templateUrl:'/pages/pushed/pushed.html',
         })
 });
+app.controller('HomeCtrl', HomeCtrl);
+    function HomeCtrl($state){
+        $state.go('home.pushed');
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
 
 module.exports = {
     mode: 'development',
-    entry: './src/app/app.js',
+    entry: {app:'./src/app/app.js', vendor: ['jquery']},
     stats: { children: false },//Entrypoint undefined = index.html
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/bundle.js'    //"bundle-[name]-[hash:8].js",
+        path: path.resolve(__dirname, 'build'),
+        filename: 'js/[name].bundle.js'    //"bundle-[name]-[hash:8].js",
     },
     module: {
         rules: [{
@@ -20,9 +23,9 @@ module.exports = {
         ]
     },
     devServer:{
-        contentBase:'/dist/',
+        contentBase:'/build/',
         host: 'localhost',//服务主机
-        historyApiFallback:true,
+        historyApiFallback: false,
         hot:true,
         inline:true,
         progress:true,
@@ -35,9 +38,19 @@ module.exports = {
             filename: 'index.html', // dist目录下生成的文件名
             template: './src/index.html' // 我们原来的index.html，作为模板
         }),
+        new CopyWebpackPlugin([ 
+        // 复制插件
+        {   from: path.join(__dirname,'src/app/pages'), 
+            to: path.join(__dirname,'build/pages') }
+        ]),
         new MiniCssExtractPlugin({
             filename: "css/[name].css",
             chunkFilename: "[id].css"
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
         }),
         //自动启动浏览器
         new OpenBrowserPlugin({ url: 'http://localhost:8087' })
